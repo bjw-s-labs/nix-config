98296bee986edf8af80300c27b0eff350b1c051a
Bernd Schorgers <me@bjw-s.dev>
2024-01-21 09:16:50 +0100

    fix(fish): Make sure `~/.local/bin` is in PATH

--- modules/common/shell/fish/default.nix
+++ modules/common/shell/fish/default.nix
@@ -24,6 +24,12 @@ in {
       ]);
     }
 
+    {
+      programs.fish.interactiveShellInit = ''
+        fish_add_path ~/.local/bin
+      '';
+    }
+
     (import ../packages/default.nix args)
 
     # Fix for https://github.com/LnL7/nix-darwin/issues/122
