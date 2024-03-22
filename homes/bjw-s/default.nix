d8875eef552fe907708c8e3e0627b0142d46256c
Bernd Schorgers <me@bjw-s.dev>
2024-03-22 11:44:30 +0100

    feat: Make vscode config user-specific instead of host-specific

--- homes/bjw-s/default.nix
+++ homes/bjw-s/default.nix
@@ -1,5 +1,7 @@
 {
   pkgs,
+  lib,
+  inputs,
   hostname,
   flake-packages,
   ...
@@ -18,6 +20,47 @@
         enable = true;
         package = flake-packages.${pkgs.system}.nvim;
         makeDefaultEditor = true;
+      };
+
+      vscode = {
+        userSettings = lib.importJSON ./config/editor/vscode/settings.json;
+        extensions = let
+          inherit (inputs.nix-vscode-extensions.extensions.${pkgs.system}) vscode-marketplace;
+        in
+          with vscode-marketplace; [
+            # Themes
+            catppuccin.catppuccin-vsc
+            thang-nm.catppuccin-perfect-icons
+
+            # Language support
+            golang.go
+            hashicorp.terraform
+            jnoortheen.nix-ide
+            mrmlnc.vscode-json5
+            ms-azuretools.vscode-docker
+            ms-python.python
+            redhat.ansible
+            redhat.vscode-yaml
+            tamasfe.even-better-toml
+
+            # Formatters
+            esbenp.prettier-vscode
+
+            # Linters
+            davidanson.vscode-markdownlint
+            fnando.linter
+
+            # Remote development
+            ms-vscode-remote.remote-containers
+            ms-vscode-remote.remote-ssh
+
+            # Other
+            gruntfuggly.todo-tree
+            ionutvmi.path-autocomplete
+            luisfontes19.vscode-swissknife
+            ms-kubernetes-tools.vscode-kubernetes-tools
+            shipitsmarter.sops-edit
+          ];
       };
     };
 
--- homes/bjw-s/hosts/bernd-macbook.nix
+++ homes/bjw-s/hosts/bernd-macbook.nix
@@ -1,48 +1,7 @@
 {
-  pkgs,
   lib,
-  inputs,
   ...
 }:
-let
-  extensions = let
-    inherit (inputs.nix-vscode-extensions.extensions.${pkgs.system}) vscode-marketplace;
-  in
-    with vscode-marketplace; [
-      # Themes
-      catppuccin.catppuccin-vsc
-      thang-nm.catppuccin-perfect-icons
-
-      # Language support
-      golang.go
-      hashicorp.terraform
-      jnoortheen.nix-ide
-      mrmlnc.vscode-json5
-      ms-azuretools.vscode-docker
-      ms-python.python
-      redhat.ansible
-      redhat.vscode-yaml
-      tamasfe.even-better-toml
-
-      # Formatters
-      esbenp.prettier-vscode
-
-      # Linters
-      davidanson.vscode-markdownlint
-      fnando.linter
-
-      # Remote development
-      ms-vscode-remote.remote-containers
-      ms-vscode-remote.remote-ssh
-
-      # Other
-      gruntfuggly.todo-tree
-      ionutvmi.path-autocomplete
-      luisfontes19.vscode-swissknife
-      ms-kubernetes-tools.vscode-kubernetes-tools
-      shipitsmarter.sops-edit
-    ];
-in
 {
   modules = {
     deployment.nix.enable = true;
@@ -51,7 +10,6 @@ in
       vscode = {
         enable = true;
         userSettings = lib.importJSON ../config/editor/vscode/settings.json;
-        extensions = extensions;
       };
     };
     kubernetes.enable = true;
