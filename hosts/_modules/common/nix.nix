49e893f44e63a8bd2651bcd5a99052ef501eb5ec
Bernd Schorgers <me@bjw-s.dev>
2024-04-02 20:05:14 +0200

    feat: Re-add nix-build workflow (#6)

--- hosts/_modules/common/nix.nix
+++ hosts/_modules/common/nix.nix
@@ -10,12 +10,14 @@
         "https://nix-community.cachix.org"
         "https://cache.garnix.io"
         "https://numtide.cachix.org"
+        "https://bjw-s.cachix.org"
       ];
 
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
         "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+        "bjw-s.cachix.org-1:dWyzjMYlnKeq01PplRWadakXfZQBoxJN7zGO6/HwsPs="
       ];
 
       # Fallback quickly if substituters are not available.
