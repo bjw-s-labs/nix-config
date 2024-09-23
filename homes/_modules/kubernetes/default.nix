d1631e44cfa8f549ff1b8106f396b344f64bde8d
Bernd Schorgers <me@bjw-s.dev>
2024-09-23 10:08:18 +0200

    fix: Fix helm plugin integration with helmfile

--- homes/_modules/kubernetes/default.nix
+++ homes/_modules/kubernetes/default.nix
@@ -6,6 +6,20 @@
 }:
 let
   cfg = config.modules.kubernetes;
+
+  # https://nixos.wiki/wiki/Helm_and_Helmfile
+  wrappedHelmPkg = (pkgs.unstable.wrapHelm pkgs.unstable.kubernetes-helm {
+    plugins = with pkgs.unstable.kubernetes-helmPlugins; [
+      helm-diff
+      helm-git
+      helm-s3
+      helm-secrets
+      helm-unittest
+    ];
+  });
+  wrappedHelmfilePkg = pkgs.unstable.helmfile-wrapped.override {
+    inherit (wrappedHelmPkg) pluginsDir;
+  };
 in
 {
   options.modules.kubernetes = {
@@ -23,7 +37,6 @@ in
     ]) ++
     (with pkgs.unstable; [
       fluxcd
-      helmfile
       krew
       kubecm
       kubeconform
@@ -31,14 +44,13 @@ in
       kubectl-neat
       kubectl-node-shell
       kubectl-view-secret
-      (wrapHelm kubernetes-helm {
-        plugins = [
-          kubernetes-helmPlugins.helm-unittest
-        ];
-      })
       stern
       talosctl
-    ]);
+    ]) ++
+    [
+      wrappedHelmPkg
+      wrappedHelmfilePkg
+    ];
 
     programs.k9s = {
       enable = true;
